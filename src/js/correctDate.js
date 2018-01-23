c42f36513aecba4aa1e3eb714618e3af29baa0ef
Pahan.Z <zagvzdin.pakhan@rambler.ru>
2018-01-23 22:36:20 +0300

    render

---
+++ src/js/correctDate.js
@@ -0,0 +1,25 @@
+export default (element) => {
+  const date = new Date(element);
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let seconds = date.getSeconds();
+  let day = date.getDay();
+  let month = date.getMonth();
+  const year = date.getFullYear();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  if (day < 10) {
+    day = `0${minutes}`;
+  }
+  if (month < 10) {
+    month = `0${seconds}`;
+  }
+  return (`${day}.${month}.${year} ${hours}:${minutes}:${seconds}`);
+};
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -2,9 +2,75 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../styles.scss';
 
-const Hello = () => {
-  const check = 'yes';
-  return check;
+import data from './data.json';
+import Correctdate from './correctDate';
+
+const ShowCarrier = (props) => {
+  const items = props.data.map(element => <option key={element.id}>{element.carrier}</option>);
+  items.unshift(<option key="allCompamies">{props.title}</option>);
+  return items;
 };
 
-ReactDOM.render(<Hello />, document.getElementById('app'));
+const RenderFrom = () => (
+  <div>
+    <RenderFromInfo data={data.flights} title="Отправление:" />
+  </div>
+);
+
+const RenderFromInfo = (props) => {
+  const items = props.data.map(element => <div key={element.id}>{element.direction.from}</div>);
+  items.unshift(<div key="from">{props.title}</div>);
+  return items;
+};
+
+const RenderToInfo = (props) => {
+  const items = props.data.map(element => <div key={element.id}>{element.direction.to}</div>);
+  items.unshift(<div key="to">{props.title}</div>);
+  return items;
+};
+
+const RenderTo = () => (
+  <div>
+    <RenderToInfo data={data.flights} title="Прибытие:" />
+  </div>
+);
+
+const RenderArrivalInfo = (props) => {
+  const items = props.data.map(element => (
+    <div key={element.id}>{Correctdate(element.arrival)}</div>));
+  items.unshift(<div key="arrival">{props.title}</div>);
+  return items;
+};
+
+const RenderArrival = () => (
+  <div>
+    <RenderArrivalInfo data={data.flights} title="Прибытие в" />
+  </div>
+);
+
+const RenderDepartureInfo = (props) => {
+  const items = props.data.map(element => (
+    <div key={element.id}>{Correctdate(element.departure)}</div>));
+  items.unshift(<div key="departure">{props.title}</div>);
+  return items;
+};
+
+const RenderDeparture = () => (
+  <div>
+    <RenderDepartureInfo data={data.flights} title="Отправление в" />
+  </div>
+);
+
+const App = () => (
+  <div className="container">
+    <select>
+      <ShowCarrier data={data.flights} title="Все авиакомпании" />
+    </select>
+    <RenderFrom />
+    <RenderTo />
+    <RenderArrival />
+    <RenderDeparture />
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('app'));
