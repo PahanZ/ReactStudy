0f8196395762060f564052baad5ec40fa38baa33
PahanZ <zagvzdin.pakhan@rambler.ru>
2018-01-26 13:51:22 +0300

    done without cleaning

--- src/js/data/getData.js
+++ src/js/data/getData.js
@@ -1,6 +1,16 @@
+import { createStore } from 'redux';
 import data from './data.json';
 
-const getData = info => info;
+const getData = info => info.flights;
 const info = getData(data);
 
-export default info;
+const reducer = (state = info, action) => {
+  if (action.type === 'changeStore') {
+    return action.change;
+  }
+  return state;
+};
+
+const store = createStore(reducer);
+
+export { store, info };
--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,61 +1,56 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import PropTypes from 'prop-types';
-import { createStore } from 'redux';
+// import PropTypes from 'prop-types';
+import { info, store } from './data/getData';
 import '../styles.scss';
 
 import correctDate from './data/correctDate';
-import info from './data/getData';
 import Select from './components/Select.jsx';
 import Cards from './components/Cards.jsx';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { carriers: info.flights };
     this.onChange = this.onChange.bind(this);
   }
   onChange(event) {
-    if (event.target.value === 'All Carrier') {
-      this.setState({ carriers: info.flights });
-    } else {
-      const newState = info.flights.filter(element => (
+    const filter = (carriers) => {
+      if (event.target.value === 'All Carrier') {
+        return info;
+      }
+      const newState = carriers.filter(element => (
         (element.carrier === event.target.value) ? element : null));
-      this.setState({ carriers: newState });
-    }
+      return newState;
+    };
+    store.dispatch({
+      type: 'changeStore',
+      change: filter(info),
+    });
   }
   render() {
-
-
-    const reducer = (check = [], action) => {
-      console.log('check action', action);
-      return check;
-    };
-    const store = createStore(reducer);
-    store.subscribe(() => {
-      console.log('check subscribe', store.getState());
-    });
-    store.dispatch({ type: 'check' });
-
     return (
       <div className="container">
         <div>
           <Select
-            data={info.flights}
+            data={info}
             title="All Carrier"
             onChange={this.onChange}
           />
         </div>
-        <Cards data={this.state.carriers} correctdate={correctDate} />
+        <Cards data={store.getState()} correctdate={correctDate} />
       </div>
     );
   }
 }
 
-Select.propTypes = {
-  data: PropTypes.array,
-  title: PropTypes.string,
-  onChange: PropTypes.func,
-};
+// Select.propTypes = {
+//   data: PropTypes.array,
+//   title: PropTypes.string,
+//   onChange: PropTypes.func,
+// };
+
+store.subscribe(() => {
+  ReactDOM.render(<App />, document.getElementById('app'));
+});
 
 ReactDOM.render(<App />, document.getElementById('app'));
