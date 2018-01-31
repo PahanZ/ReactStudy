eb81d6a653236ee1bd0ec345c1e4cf54e16408a4
Pahan.Z <zagvzdin.pakhan@rambler.ru>
2018-02-01 00:59:55 +0300

    edits

--- src/js/redux/actions.js
+++ src/js/redux/actions.js
@@ -1,11 +1,15 @@
-const changeCompany = data => ({
-  type: 'setCompanyFilter',
-  payload: data,
-});
+const changeCompany = data => (dispatch) => {
+  dispatch({
+    type: 'setCompanyFilter',
+    payload: data,
+  });
+};
 
-const downloadData = data => ({
-  type: 'setFlights',
-  payload: data,
-});
+const downloadData = data => (dispatch) => {
+  dispatch({
+    type: 'setFlights',
+    payload: data,
+  });
+};
 
 export { changeCompany, downloadData };
